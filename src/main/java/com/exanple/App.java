16f1397ffe88f4dab353224420e77c2d61e0a701
Dimple KB <dimpledimpu50@gmail.com>
2026-06-18 06:17:45 +0530

    added

---
+++ src/main/java/com/exanple/App.java
@@ -0,0 +1,13 @@
+package com.exanple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
